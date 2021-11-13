3fbdc70fc4f7830a47e410e5d73bdc37e840b1f0
Feroz Syariff <ferozqcs@gmail.com>
2021-11-13 15:35:36 +0800

    Commiting initial git repo

---
+++ src/main/java/com/saveit/expendituretracker/ExpenditureTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.saveit.expendituretracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenditureTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenditureTrackerApplication.class, args);
+	}
+
+}
